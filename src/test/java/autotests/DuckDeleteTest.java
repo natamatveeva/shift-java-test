cd9809efa4f2de2a1e2b37be523bfc7924c68596
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-12-07 09:02:02 +0300

    создан тестовый класс для удаления утки

---
+++ src/test/java/autotests/DuckDeleteTest.java
@@ -0,0 +1,67 @@
+package autotests;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckDeleteTest extends TestNGCitrusSpringSupport {
+    @Test(description="Удаление утки")
+    @CitrusTest
+    public void successfulDelete(@Optional @CitrusResource TestCaseRunner runner) {
+        String color = "yellow";
+        double height = 0.3;
+        String material = "rubber";
+        String sound = "qack";
+        String wingsState = "FIXED";
+//        createDuck(runner, color, height, material, sound, wingsState);
+        deleteDuck(runner, "10");
+        validateResponse(runner, "{\n" +
+                                 "\"message\":" + "\"Duck is deleted\"\n" +
+                                 "}");
+
+    }
+
+    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
+        runner.$(
+                http()
+                        .client("http://localhost:2222")
+                        .send()
+                        .post("/api/duck/create")
+                        .message().contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .body("{\n" +
+                              "\"color\":\"" + color + "\",\n" +
+                              "\"height\":" + height + ",\n" +
+                              "\"material\":\"" + material + "\",\n" +
+                              "\"sound\":\"" + sound + "\",\n" +
+                              "\"wingsState\":\"" + wingsState + "\"\n" +
+                              "}")
+        );
+    }
+    public void deleteDuck(TestCaseRunner runner, String id) {
+        runner.$(
+                http()
+                        .client("http://localhost:2222")
+                        .send()
+                        .delete("/api/duck/delete")
+                        .queryParam("id", id));
+
+    }
+    public void validateResponse(TestCaseRunner runner, String responseMessage) {
+        runner.$(
+                http()
+                        .client("http://localhost:2222")
+                        .receive()
+                        .response(HttpStatus.OK)
+                        .message()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .body(responseMessage)
+        );
+    }
+}
