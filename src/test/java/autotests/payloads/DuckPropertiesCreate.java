a8d741e9f87c8f745c1ef3b64f4b3ec533a00c18
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-12-10 13:29:23 +0300

    создан payloads для создания утки

---
+++ src/test/java/autotests/payloads/DuckPropertiesCreate.java
@@ -0,0 +1,32 @@
+package autotests.payloads;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(fluent = true)
+public class DuckPropertiesCreate {
+    @JsonProperty("color")
+    private String color;
+
+    @JsonProperty("height")
+    private double height;
+
+    @JsonProperty("material")
+    private String material;
+
+    @JsonProperty("sound")
+    private String sound;
+
+    @JsonProperty("wingsState")
+    private WingsState wingsState;
+
+    public enum WingsState {
+        ACTIVE,
+        FIXED,
+        UNDEFINED
+    }
+}
