580c6adb2a2d719b9a2d3fadbe4ea8d8473298d1
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-12-11 12:18:56 +0300

    рефакторинг "обновить"

--- src/test/java/autotests/DuckUpdateTest.java
+++ src/test/java/autotests/DuckUpdateTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
+import static com.consol.citrus.container.FinallySequence.Builder.doFinally;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 import static com.consol.citrus.validation.DelegatingPayloadVariableExtractor.Builder.fromBody;
 
@@ -20,48 +21,45 @@ public class DuckUpdateTest extends DuckActionsClient {
 
     @Test(description = "Обновление параметров утки: высота и цвет")
     @CitrusTest
-    public void updateDuckParametersColorHeight(@Optional @CitrusResource TestCaseRunner runner,
-                                                @Optional @CitrusResource TestContext context
-                                                ) {
-        DuckPropertiesCreate duckProperties = new DuckPropertiesCreate()
-                .color("yellow")
-                .height(0.3)
-                .material("rubber")
-                .sound("quack")
-                .wingsState(DuckPropertiesCreate.WingsState.ACTIVE);
-        createDuck(runner, duckProperties);
-        String id = extractIdDuckFromResponse(runner, context);
-        updateDuck(runner, id,
+    public void updateDuckParametersColorHeight(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","10");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'orange', 3.0, 'rubber', 'quack','ACTIVE');");
+        updateDuck(runner, "${duckId}",
                 "white",
                 3.5,
                 "rubber",
                 "quack",
                 "ACTIVE");
         ResponseMessage message = new ResponseMessage()
-                .message("{\n\"message\": \"Duck with id = " + id + " is updated\"\n}");
+                .message("{\n\"message\": \"Duck with id = " + "${duckId}" + " is updated\"\n}");
         validateResponse(runner, message.message());
+        validateDuckInDatabase(runner, "${duckId}", "white", "3.5", "rubber", "quack", "ACTIVE");
+
     }
 
     @Test(description = "Обновление параметров утки: цвет и звук")
     @CitrusTest
-    public void updateDuckParametersColorSound(@Optional @CitrusResource TestCaseRunner runner,
-                                               @Optional @CitrusResource TestContext context) {
-        DuckPropertiesCreate duckProperties = new DuckPropertiesCreate()
-                .color("yellow")
-                .height(0.3)
-                .material("rubber")
-                .sound("quack")
-                .wingsState(DuckPropertiesCreate.WingsState.ACTIVE);
-        createDuck(runner, duckProperties);
-        String id = extractIdDuckFromResponse(runner, context);
-        updateDuck(runner, id,
+    public void updateDuckParametersColorSound(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","11");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'orange', 3.0, 'rubber', 'quack','ACTIVE');");
+        updateDuck(runner, "${duckId}",
                 "white",
-                1,
-                "metall",
+                3.0,
+                "rubber",
                 "qa",
                 "ACTIVE");
         validateResponse(runner, "{\n" +
-                                 "\"message\": \"Duck with id = " + id + " is updated\"\n" +
+                                 "\"message\": \"Duck with id = " + "${duckId}" + " is updated\"\n" +
                                  "}");
+        validateDuckInDatabase(runner, "${duckId}", "white", "3.0", "rubber", "qa", "ACTIVE");
+
     }
 }
