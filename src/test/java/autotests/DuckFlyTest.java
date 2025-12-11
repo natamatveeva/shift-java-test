dfd24bfae6e3055a96f722f12162356737dab015
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-12-11 12:04:33 +0300

    рефакторинг "лететь"

--- src/test/java/autotests/DuckFlyTest.java
+++ src/test/java/autotests/DuckFlyTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
+import static com.consol.citrus.container.FinallySequence.Builder.doFinally;
 import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
@@ -20,18 +21,14 @@ public class DuckFlyTest extends DuckActionsClient {
 
     @Test(description = "Проверка полета с активными крыльями")
     @CitrusTest
-    public void successfulFly(@Optional @CitrusResource TestCaseRunner runner,
-                              @Optional @CitrusResource TestContext context
-                              ) {
-        DuckPropertiesCreate duckProperties = new DuckPropertiesCreate()
-                .color("yellow")
-                .height(0.3)
-                .material("rubber")
-                .sound("quack")
-                .wingsState(DuckPropertiesCreate.WingsState.ACTIVE);
-        createDuck(runner, duckProperties);
-        String id = extractIdDuckFromResponse(runner, context);
-        flyDuck(runner, id);
+    public void successfulFly(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","4");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'white', 3.0, 'rubber', 'quack','ACTIVE');");
+        flyDuck(runner, "${duckId}");
         validateResponse(runner, "{\n" +
                                  "\"message\":" + "\"I am flying :)\"\n" +
                                  "}");
@@ -39,37 +36,29 @@ public class DuckFlyTest extends DuckActionsClient {
 
     @Test(description = "Проверка полета с неактивными крыльями")
     @CitrusTest
-    public void unsuccessfulFly(@Optional @CitrusResource TestCaseRunner runner,
-                                @Optional @CitrusResource TestContext context
-                                ) {
-        DuckPropertiesCreate duckProperties = new DuckPropertiesCreate()
-                .color("yellow")
-                .height(0.3)
-                .material("rubber")
-                .sound("quack")
-                .wingsState(DuckPropertiesCreate.WingsState.FIXED);
-        createDuck(runner, duckProperties);
-        String id = extractIdDuckFromResponse(runner, context);
-        flyDuck(runner, id);
+    public void unsuccessfulFly(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","5");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'white', 3.0, 'rubber', 'quack','FIXED');");
+        flyDuck(runner, "${duckId}");
         validateResponse(runner, "{\n" +
                                  "\"message\":" + "\"I can not fly :C\"\n" +
                                  "}");
     }
 
-    @Test(description = "Проверка полета с определенными крыльями")
+    @Test(description = "Проверка полета с неопределенными крыльями")
     @CitrusTest
-    public void undefinedFly(@Optional @CitrusResource TestCaseRunner runner,
-                             @Optional @CitrusResource TestContext context
-                            ) {
-        DuckPropertiesCreate duckProperties = new DuckPropertiesCreate()
-                .color("yellow")
-                .height(0.3)
-                .material("rubber")
-                .sound("quack")
-                .wingsState(DuckPropertiesCreate.WingsState.UNDEFINED);
-        createDuck(runner, duckProperties);
-        String id = extractIdDuckFromResponse(runner, context);
-        flyDuck(runner, id);
+    public void undefinedFly(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","6");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'white', 3.0, 'rubber', 'quack','UNDEFINED');");
+        flyDuck(runner, "${duckId}");
         validateResponse(runner, "{\n" +
                                  "\"message\":" + "\"Wings are not detected :(\"\n" +
                                  "}");
