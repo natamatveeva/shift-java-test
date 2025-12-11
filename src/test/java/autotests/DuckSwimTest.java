bfe5377e753163ddf9ffb4ec7134494baee6da5d
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-12-11 09:52:22 +0300

    новый тест "плыть"

--- src/test/java/autotests/DuckSwimTest.java
+++ src/test/java/autotests/DuckSwimTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
+import static com.consol.citrus.container.FinallySequence.Builder.doFinally;
 import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
@@ -41,7 +42,20 @@ public class DuckSwimTest extends DuckActionsClient {
                                       @Optional @CitrusResource TestContext context) {
         swimDuck(runner, "0");
         validateResponse(runner, "{\n" +
-                                  "\"message\":" + "\"Paws are not found ((((\"\n" +
+                                 "\"message\":" + "\"Paws are not found ((((\"\n" +
                                  "}");
+    }
+
+    @Test(description = "Уточка плыви (корректный id)")
+    @CitrusTest
+    public void successfullSwimDb(@Optional @CitrusResource TestCaseRunner runner) {
+        runner.variable("duckId","1");
+        runner.$(doFinally().actions(context ->
+                databaseUpdate(runner, "DELETE FROM DUCK WHERE ID=${duckId}")));
+        databaseUpdate(runner,
+                "insert into DUCK (id, color, height, material, sound, wings_state)\n" +
+                "values (${duckId}, 'orange', 3.0, 'cheese', 'hrum','ACTIVE');");
+        swimDuck(runner,"${duckId}");
+        validateResponseResources(runner, "DuckActionsTest/unsuccessDuckSwim.json");
     }
 }
